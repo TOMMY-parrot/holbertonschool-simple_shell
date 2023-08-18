81fde59252b7b1d98be9dc8df4bdb7ee04742e4d
TOMMY-parrot <yassinguellouz@gmail.com>
2023-08-18 05:59:13 -0700

    lists the environmntale variables

---
+++ list_env.c
@@ -0,0 +1,21 @@
+#include "my_shell.h"
+
+
+/**
+ * custom_getenv - prints env to the  output
+ *
+ * @env: enviroment
+ *
+ */
+
+void custom_getenv(char **env)
+{
+	size_t s = 0;
+
+	while (env[s])
+	{
+		write(STDOUT_FILENO, env[s], _strlen(env[s]));
+		write(STDOUT_FILENO, "\n", 1);
+		s++;
+	}
+}
